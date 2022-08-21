8fd4e999a8ba0b40a1043c88eb77e48d1e7969b6
Jayanta Roy <joyanto@invento.com.bd>
2022-08-21 12:20:27 +0600

    update: get_distriutor api updated

--- field_force/api_methods/customer.py
+++ field_force/api_methods/customer.py
@@ -6,17 +6,17 @@ from field_force.response import build_custom_response
 @frappe.whitelist()
 def get_distributors(**kwargs):
     filters = {
-        'customer_group': 'Distributor',
+        # 'customer_group': 'Distributor',
     }
 
-    if kwargs.get('search'):
-        filters['customer_name'] = ["like", f"%{kwargs.pop('search')}%"]
+    if kwargs.get('name'):
+        filters['name'] = ["like", f"%{kwargs.pop('name ')}%"]
 
     if 'cmd' in kwargs.keys():
         del kwargs['cmd']
 
-    if frappe.has_permission('Customer', 'read'):
-        distributors = frappe.db.get_list('Customer', filters, **kwargs)
+    if frappe.has_permission('Distributor', 'read'):
+        distributors = frappe.db.get_list('Distributor', filters, **kwargs)
         frappe.local.response.total_items = len(distributors)
         frappe.local.response.data = distributors
     else:
