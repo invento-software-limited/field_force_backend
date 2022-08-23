cc2df441ac14626e8076696026def088ba911cab
Jayanta Roy <joyanto@invento.com.bd>
2022-08-23 18:42:06 +0600

    fix: bug fixed on Store Visit Asign doctype

--- field_force/field_force/doctype/store_visit_assign/store_visit_assign.py
+++ field_force/field_force/doctype/store_visit_assign/store_visit_assign.py
@@ -6,7 +6,13 @@ from frappe.model.document import Document
 
 class StoreVisitAssign(Document):
 	def validate(self):
-		if frappe.db.exists('Store Visit Assign', {'employee': self.employee, 'date': self.date}):
+		filters = [
+			['name', '!=', self.name],
+			['employee', '=', self.employee],
+			['date', '=', self.date]
+		]
+
+		if frappe.db.get_list('Store Visit Assign', filters):
 			frappe.throw(f"Store Visit is already assigned for employee '{self.employee}' at '{self.date}'")
 
 		if self.destinations:
