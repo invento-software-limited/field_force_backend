8d2e8a25d32c63c48f08211f41e61b8c0f0b4f48
Jayanta Roy <joyanto@invento.com.bd>
2022-08-31 10:27:01 +0600

    update: Requisition doctype updated

--- field_force/field_force/doctype/requisition/requisition.py
+++ field_force/field_force/doctype/requisition/requisition.py
@@ -11,7 +11,6 @@ class Requisition(Document):
 		self.validate_delivery_date()
 		self.validate_items()
 
-
 	def validate_delivery_date(self):
 		if self.transaction_date > self.delivery_date:
 			frappe.throw("Expected delivery date should be after Requisition date")
@@ -20,8 +19,10 @@ class Requisition(Document):
 			if not item.delivery_date:
 				item.delivery_date = self.delivery_date
 
-
 	def validate_items(self):
+		total_items = 0
+		total_qty = 0
+		total = 0
 
 		if self.items:
 			for item in self.items:
@@ -29,20 +30,33 @@ class Requisition(Document):
 					frappe.throw(f"Please give quantity of item <b>{item.item_name}</b>")
 
 				if not item.rate and not item.amount and item.qty:
-					self.total_qty += item.qty
-
 					rate = frappe.db.get_value("Item Price", {"item_code": item.item_code, "selling": 1}, ["price_list_rate"])
 
 					if rate:
 						item.rate = rate
 						item.amount = item.qty * rate
-						self.total += item.amount
-						self.net_total = self.total
-						self.grand_total = self.net_total
+						total += item.amount
 					else:
 						frappe.throw(f"There is not selling rate of item <b>{item.item_name}</b>")
 
-				self.total_items += 1
-
+				total_items += 1
+				total_qty += item.qty
 		else:
 			frappe.throw("Items are required")
+
+		if total:
+			self.total = total
+			self.net_total = total
+			self.grand_total = total
+
+		if total_items and total_qty:
+			self.total_qty = total_qty
+			self.total_items = total_items
+
+	def on_submit(self):
+		if self.docstatus == 1:
+			self.status = "Submitted"
+
+	def on_cancel(self):
+		if self.docstatus == 2:
+			self.status = "Cancelled"
