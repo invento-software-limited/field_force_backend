c7d8c87a3ea6cb0ab0de625b6b0da672ecfcc811
Jayanta Roy <joyanto@invento.com.bd>
2022-09-07 15:08:12 +0600

    update: Store Visit, Store Visit Assign and Store Visit destination doctype updated

--- field_force/field_force/doctype/store_visit/store_visit.py
+++ field_force/field_force/doctype/store_visit/store_visit.py
@@ -5,14 +5,31 @@ import frappe
 from frappe.model.document import Document
 
 class StoreVisit(Document):
-	# def validate(self):
-	# 	if frappe.session.user != 'administrator':
-	# 		self.user = frappe.session.user
+	def validate(self):
+		# if frappe.session.user != 'administrator':
+		# 	self.user = frappe.session.user
+
+		if not self.email or not self.user_fullname:
+			email, user_fullname = frappe.db.get_value('User', self.user, ['email', 'full_name'])
+
+			if not self.email and email:
+				self.email = email
+			if not self.user_fullname and user_fullname:
+				self.user_fullname = user_fullname
+
+		if not self.contact_number or not self.customer_address:
+			contact_number, customer_address = frappe.db.get_value('Customer', self.customer, ['contact_number', 'address'])
+
+			if not self.contact_number and contact_number:
+				self.contact_number = contact_number
+			if not self.customer_address and customer_address:
+				self.customer_address = customer_address
+
 	def after_insert(self):
 		filters = {
 			"customer": self.customer,
 			"user": self.user,
-			"date": self.device_date
+			"date": self.server_date
 		}
 
 		if frappe.db.exists('Store Visit Destination', filters):
