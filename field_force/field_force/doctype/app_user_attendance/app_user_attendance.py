ef0c50c97d43875683542a63bc99f36e696aa831
Jayanta Roy <joyanto@invento.com.bd>
2022-09-07 11:47:34 +0600

    update: App User Attendance doctype updated

--- field_force/field_force/doctype/app_user_attendance/app_user_attendance.py
+++ field_force/field_force/doctype/app_user_attendance/app_user_attendance.py
@@ -5,7 +5,14 @@ import frappe
 from frappe.model.document import Document
 
 class AppUserAttendance(Document):
-	# def validate(self):
+	def validate(self):
 	# 	if frappe.session.user != 'administrator':
 	# 		self.user = frappe.session.user
-	pass
+
+		if not self.email or not self.user_fullname:
+			email, user_fullname = frappe.db.get_value('User', self.user, ['email', 'full_name'])
+
+			if not self.email and email:
+				self.email = email
+			if not self.user_fullname and user_fullname:
+				self.user_fullname = user_fullname
