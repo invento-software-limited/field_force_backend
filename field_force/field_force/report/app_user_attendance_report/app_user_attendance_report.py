4700c17ca9de9f855bdaaaa8299c100a9b7730a3
Jayanta Roy <joyanto@invento.com.bd>
2022-09-11 11:40:21 +0600

    update: App User Attendance report updated

--- field_force/field_force/report/app_user_attendance_report/app_user_attendance_report.py
+++ field_force/field_force/report/app_user_attendance_report/app_user_attendance_report.py
@@ -13,21 +13,29 @@ def execute(filters=None):
             title = app_user_attendance.user_fullname or app_user_attendance.user
             app_user_attendance['user'] = f'<a href="/app/user/{app_user_attendance.user}" target="_blank">{title}</a>'
 
+        image_url = app_user_attendance.image
+
+        if  image_url:
+            if '/files/' in image_url:
+                app_user_attendance['image'] = f'<a href="{image_url}" target="_blank">{image_url}</a>'
+
     return columns, data
 
 def get_columns(filters):
     """ Columns of Report Table"""
     columns = [
-		{"label": _("Date"), "fieldname": "server_date", "width": 140},
-		{"label": _("User"), "fieldname": "user", "width": 150, "fieldtype": "Data"},
-        {"label": _("Type"), "fieldname": "type", "width": 140},
-        {"label": _("Time"), "fieldname": "server_time", "fieldtype": "Time", "width": 130},
-        {"label": _("Device Date"), "fieldname": "device_date", "fieldtype": "Date", "width": 140},
-        {"label": _("Device Time"), "fieldname": "device_time", "fieldtype": "Time", "width": 130},
-        {"label": _("Latitude"), "fieldname": "latitude", "width": 125},
-        {"label": _("Longitude"), "fieldname": "longitude", "width": 125},
-        {"label": _("Device Model"), "fieldname": "device_model", "width": 130},
-
+		{"label": _("Date"), "fieldname": "server_date", "width": 110},
+        {"label": _("Time"), "fieldname": "server_time", "fieldtype": "Time", "width": 90},
+        {"label": _("Name"), "fieldname": "name", "fieldtype": "Link", "options": "App User Attendance",
+         "width": 120},
+        {"label": _("User"), "fieldname": "user", "width": 110, "fieldtype": "Data"},
+        {"label": _("Type"), "fieldname": "type", "width": 90},
+        {"label": _("Device Date"), "fieldname": "device_date", "fieldtype": "Date", "width": 120},
+        {"label": _("Device Time"), "fieldname": "device_time", "fieldtype": "Time", "width": 100},
+        {"label": _("Latitude"), "fieldname": "latitude", "width": 120},
+        {"label": _("Longitude"), "fieldname": "longitude", "width": 120},
+        {"label": _("Device Model"), "fieldname": "device_model", "width": 120},
+        {"label": _("Image"), "fieldname": "image", "width": 120},
     ]
 
     return columns
@@ -35,12 +43,12 @@ def get_columns(filters):
 def get_data(filters):
     conditions = get_conditions(filters)
 
-    query_string = """select app_user_attendance.user, app_user_attendance.user_fullname, 
+    query_string = """select app_user_attendance.name, app_user_attendance.user, app_user_attendance.user_fullname, 
                     app_user_attendance.server_date, app_user_attendance.server_time, app_user_attendance.type,
                     app_user_attendance.device_date, app_user_attendance.device_time, app_user_attendance.latitude,
-                    app_user_attendance.longitude, app_user_attendance.device_model
+                    app_user_attendance.longitude, app_user_attendance.device_model, app_user_attendance.image
                     from `tabApp User Attendance` app_user_attendance where %s
-                    order by app_user_attendance.device_date desc""" % (conditions)
+                    order by app_user_attendance.server_date desc""" % (conditions)
 
     query_result = frappe.db.sql(query_string, as_dict=1, debug=0)
     return query_result
@@ -57,9 +65,9 @@ def get_conditions(filters):
     if type:
         conditions.append("app_user_attendance.type = '%s'" % type)
     if from_date:
-        conditions.append("app_user_attendance.device_date >= '%s'" % from_date)
+        conditions.append("app_user_attendance.server_date >= '%s'" % from_date)
     if to_date:
-        conditions.append("app_user_attendance.device_date <= '%s'" % to_date)
+        conditions.append("app_user_attendance.server_date <= '%s'" % to_date)
     if user:
         conditions.append("app_user_attendance.user = '%s'" % user)
 
