9e0967be9c754eb24df6ec0e778b6d2a7c992f2b
Jayanta Roy <joyanto@invento.com.bd>
2022-11-14 13:35:04 +0600

    Announcement doctype created and added to dashboard api

--- field_force/api_methods/dashboard.py
+++ field_force/api_methods/dashboard.py
@@ -6,31 +6,36 @@ import calendar
 
 @frappe.whitelist()
 def get_dashboard_data():
-    today_date = datetime.datetime.today()
-    current_month, current_year = today_date.month, frappe.defaults.get_user_default('fiscal_year')
+    today = datetime.datetime.today()
+    today_date = today.date()
+    month_start_date = today.replace(day=1)
+
+    current_month, current_year = today.month, frappe.defaults.get_user_default('fiscal_year')
     current_month_name = calendar.month_name[current_month]
 
-    sales_target = get_sales_target(current_month_name, current_year)
-    daily_sales = get_daily_sales(today_date.date())
-    daily_customer = get_daily_customers(today_date.date())
-
-    print(daily_customer)
+    sales_target = get_sales_target(current_month_name, current_year,  month_start_date, today_date)
+    daily_sales = get_daily_sales(today_date)
+    daily_customers = get_daily_customers(today_date)
+    monthly_customers = get_monthly_customers(month_start_date, today_date)
 
     dashboard_data = {
-        "month": current_month,
+        "month": current_month_name,
         "year": current_year,
-        "monthly_target": sales_target.target_amount if sales_target else 0,
-        "monthly_sales": sales_target.achievement_amount if sales_target else 0,
+        "daily_sold_items": get_daily_sold_items(today_date),
         "daily_sales": daily_sales.total if daily_sales else 0,
-        "daily_customers": daily_customer.total if daily_customer else 0,
-        "monthly_customer": 0,
+        "daily_customers": daily_customers.total if daily_customers else 0,
+        "monthly_target": sales_target.target_amount if sales_target else 0,
+        # "monthly_sales": sales_target.achievement_amount if sales_target else 0,
+        "monthly_sales": get_monthly_sales(month_start_date, today_date),
+        "monthly_customers": monthly_customers.total if monthly_customers else 0,
+        "announcements": get_announcements(today_date)
     }
 
     frappe.local.response.total_items = 0
     frappe.local.response.data = dashboard_data
     return build_custom_response(response_type='custom')
 
-def get_sales_target(month, year):
+def get_sales_target(month, year, month_start_date, today_date):
     filters = {
         "user": frappe.session.user,
         "month": month,
@@ -39,24 +44,61 @@ def get_sales_target(month, year):
 
     if frappe.db.exists("Sales Person Target", filters):
         sales_target = frappe.get_doc("Sales Person Target", filters)
+
+        # if not sales_target.achievement_amount:
+        #     sales_target.achievement_amount = get_monthly_sales(month_start_date, today_date)
+        #     sales_target.save()
+
         return sales_target
 
     return None
 
 def get_daily_sales(date):
     daily_sales = frappe.db.sql("""select name, sum(grand_total) as total from `tabSales Order` where user='%s'
-                                and transaction_date='%s' group by user""" % (frappe.session.user, date), as_dict=1)
+                                 and docstatus!=2 and transaction_date='%s' group by user""" % (frappe.session.user,
+                                                                                                date), as_dict=1)
 
     if daily_sales:
         return daily_sales[0]
+    return 0
 
+def get_monthly_sales(month_start_date, today_date):
+    monthly_sales = frappe.db.sql("""select name, sum(grand_total) as total from `tabSales Order` where user='%s'
+                                 and docstatus!=2 and transaction_date between '%s' and '%s' group by user""" %
+                                  (frappe.session.user, month_start_date, today_date), as_dict=1)
+    if monthly_sales:
+        return monthly_sales[0].total
     return 0
 
 def get_daily_customers(date):
-    daily_customer = frappe.db.sql("""select count(distinct customer) as total from `tabSales Order`
-                                where user='%s' and transaction_date='%s' group by 
+    daily_customers = frappe.db.sql("""select count(distinct customer) as total from `tabSales Order`
+                                where user='%s' and docstatus!=2 and transaction_date='%s' group by 
                                 user""" % (frappe.session.user, date), as_dict=1)
 
-    if daily_customer:
-        return daily_customer[0]
+    if daily_customers:
+        return daily_customers[0]
     return None
+
+def get_monthly_customers(month_start_date, today_date):
+    monthly_customers = frappe.db.sql("""select count(distinct customer) as total from `tabSales Order`
+                                where user='%s' and docstatus!=2 and transaction_date between '%s' and '%s' group by 
+                                user""" % (frappe.session.user, month_start_date, today_date), as_dict=1)
+
+    if monthly_customers:
+        return monthly_customers[0]
+    return None
+
+def get_daily_sold_items(date):
+    daily_items = frappe.db.sql("""select sum(total_items) as total from `tabSales Order` where user='%s' and 
+                                    docstatus!=2 and transaction_date='%s' group by 
+                                   user""" % (frappe.session.user, date), as_dict=1)
+    if daily_items:
+        return daily_items[0].total
+    return 0
+
+def get_announcements(date):
+    announcements = frappe.db.sql("""select name, announcement_date, announcement_message from `tabAnnouncement` where
+                                    disabled=0 and from_date <= '%s' and to_date >= '%s'  
+                                    order by announcement_date desc, creation desc """ % (date, date), as_dict=1)
+
+    return announcements
