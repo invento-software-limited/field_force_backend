40491a07db680c1dd33971a96d67fb4c0bfdc934
Jayanta Roy <joyanto@invento.com.bd>
2022-12-15 17:27:34 +0600

    add: Sales person added to App User Attendance

--- field_force/field_force/doctype/app_user_attendance/app_user_attendance.py
+++ field_force/field_force/doctype/app_user_attendance/app_user_attendance.py
@@ -19,11 +19,19 @@ class AppUserAttendance(Document):
 			if not self.user_fullname and user_fullname:
 				self.user_fullname = user_fullname
 
-		set_cheat_status(self)
+		# if not self.company:
+		# 	self.company = frappe.defaults.get_user_defaults('Company')
+
+		self.set_sales_person()
 		self.set_employee()
+		set_cheat_status(self)
 
 	def before_save(self):
 		set_location_to_map(self)
+
+	def set_sales_person(self):
+		if self.user and not self.sales_person and frappe.db.exists("Sales Person", {"user": self.user}):
+			self.sales_person = frappe.db.get_value("Sales Person", {"user": self.user}, "name")
 
 	def set_employee(self):
 		if self.owner and not self.employee:
