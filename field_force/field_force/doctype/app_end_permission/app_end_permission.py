cb4dbaef68fef9d8016177db60735d37a539e7b1
Jayanta Roy <joyanto@invento.com.bd>
2022-12-18 15:17:31 +0600

    update: App End Permission doctype updated

--- field_force/field_force/doctype/app_end_permission/app_end_permission.py
+++ field_force/field_force/doctype/app_end_permission/app_end_permission.py
@@ -8,22 +8,22 @@ class AppEndPermission(Document):
 	def validate(self):
 		if self.permissions:
 			for permission in self.permissions:
-				if frappe.db.exists('App End Permission Role Profile', {'role_profile': permission.role_profile}):
-					frappe.throw(f"App End Permissions already exist for <b>{permission.role_profile}.</b>")
+				permission.doctypes = permission.doctypes.replace(" ", "")
 
-				doctypes = permission.doctypes.replace(" ", "").split(',')
+				if self.check_validation:
+					if frappe.db.exists('App End Permission Role Profile', {'name':['!=', permission.name],
+																			'role_profile': permission.role_profile}):
+						frappe.throw(f"App End Permissions already exist for <b>{permission.role_profile}.</b>")
 
-				for doctype in doctypes:
-					doctype = doctype.replace("_", " ")
+					doctypes = permission.doctypes.split(',')
 
-					if doctype and not frappe.db.exists("Doctype", {'name': doctype}):
-						frappe.throw(f"<b>{permission.role_profile}:</b> '{doctype}' doctype not found!")
+					for doctype in doctypes:
+						doctype = doctype.replace("_", " ")
+
+						if doctype and not frappe.db.exists("Doctype", {'name': doctype}):
+							frappe.throw(f"<b>{permission.role_profile}:</b> '{doctype}' doctype not found!")
 		# else:
 		# 	frappe.throw("Must have one role permission")
 
-
-	def save(self, *args, **kwargs):
-		super().save(*args, **kwargs)
-
-
-			
+	# def save(self, *args, **kwargs):
+	# 	super().save(*args, **kwargs)
