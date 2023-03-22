37d49229435010955486291c28c28c0fae0bee98
Jayanta Roy <joyanto@invento.com.bd>
2023-03-22 18:26:58 +0600

    update: Store Visit Destinaiton updated

--- field_force/field_force/doctype/store_visit_assign/store_visit_assign.js
+++ field_force/field_force/doctype/store_visit_assign/store_visit_assign.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Store Visit Assign', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+        console.log("it works!");
+	}
+});
 
-	// }
+frappe.ui.form.on("Store Visit Destination", {
+	refresh: function(frm) {
+        console.log("child table!")
+	},
+    exp_hour: function(frm, cdt, cdn) {
+        console.log(locals[cdt][cdn])
+    },
+    exp_minute: function(frm, cdt, cdn){
+        console.log(locals[cdt][cdn])
+    },
+    exp_format: function(frm, cdt, cdn){
+        console.log(locals[cdt][cdn])
+    },
+    time_till_hour: function(frm, cdt, cdn){
+        console.log(locals[cdt][cdn])
+    },
+    time_till_minute: function(frm, cdt, cdn){
+        console.log(locals[cdt][cdn])
+    },
+    time_till_format: function(frm, cdt, cdn){
+        console.log(locals[cdt][cdn])
+    }
 });
