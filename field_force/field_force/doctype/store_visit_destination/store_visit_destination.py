6fda3283f4c7b521545374ea975d084c45d6ed51
Jayanta Roy <joyanto@invento.com.bd>
2023-08-07 18:01:14 +0600

    add: store visit destination doctype updated

--- field_force/field_force/doctype/store_visit_destination/store_visit_destination.py
+++ field_force/field_force/doctype/store_visit_destination/store_visit_destination.py
@@ -3,7 +3,8 @@
 
 import frappe
 from frappe.model.document import Document
-from field_force.field_force.page.utils import get_spent_time
+from field_force.field_force.page.utils import get_timedelta_time_obj
+
 
 class StoreVisitDestination(Document):
     def validate(self):
@@ -13,7 +14,7 @@ class StoreVisitDestination(Document):
             self.checkout_time = None
 
         if self.checkin_time and self.checkout_time:
-            self.spent_time = get_spent_time(self.checkin_time, self.checkout_time, in_word=False)
+            self.spent_time = get_spent_time(self.checkin_time, self.checkout_time, in_word=True)
 
         self.set_status()
 
@@ -31,3 +32,30 @@ class StoreVisitDestination(Document):
         if status:
             frappe.db.set_value(self.parenttype, self.parent, 'status', status)
             frappe.db.commit()
+
+
+def get_spent_time(in_time, out_time, in_word=True):
+    if not in_time and not out_time:
+        return ''
+
+    if isinstance(in_time, str):
+        in_time = get_timedelta_time_obj(in_time)
+    if isinstance(out_time, str):
+        out_time = get_timedelta_time_obj(out_time)
+
+    spent_time = out_time - in_time
+
+    if in_word:
+        time_elements = str(spent_time).split(':')
+        hour, minutes = int(time_elements[0]), int(time_elements[1])
+        spent_time = ''
+
+        if hour:
+            spent_time = f"{hour}h" if hour > 1 else f"{hour}h"
+        if minutes:
+            if spent_time:
+                spent_time += " "
+
+            spent_time += f" {minutes}m" if minutes > 1 else f"{minutes}m"
+
+    return spent_time
