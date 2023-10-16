eaca2014169e3bf8844f947708d74f42cfd33976
Jayanta Roy <joyanto@invento.com.bd>
2023-10-16 15:57:51 +0600

    fix: permission check issue

--- field_force/field_force/doctype/requisition/requisition_list.js
+++ field_force/field_force/doctype/requisition/requisition_list.js
@@ -1,5 +1,8 @@
 function check_role(){
-  if (frappe.user.has_role("Customer") && !frappe.user.has_role("System Manager")
+  console.log(frappe.user.has_role("Customer"), !has_common(frappe.user_roles, ["System Manager"]),
+    !window.location.href.includes('/customer-requisition/'));
+
+  if (frappe.user.has_role("Customer") && !has_common(frappe.user_roles, ["System Manager"])
       && !window.location.href.includes('/customer-requisition/')) {
       let url = window.location.href;
 	    window.location.href = url.replace("/requisition/", "/customer-requisition/");
@@ -7,12 +10,16 @@ function check_role(){
 }
 
 frappe.listview_settings['Requisition'] = {
-  onload: function (){
+  refresh: function (){
     check_role();
   },
   primary_action: function () {
-    frappe.new_doc("Requisition");
-			let url = window.location.href;
-			window.location.href = url.replace("/requisition/", "/customer-requisition/");
+      frappe.new_doc("Requisition");
+
+      if (frappe.user.has_role("Customer") && !frappe.user.has_role("System Manager")
+      && !window.location.href.includes('/customer-requisition/')) {
+        let url = window.location.href;
+        window.location.href = url.replace("/requisition/", "/customer-requisition/");
+      }
   }
 }
