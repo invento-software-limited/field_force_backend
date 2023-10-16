3bde7f604c90b0da6eb472047c606f1126fc2df6
Jayanta Roy <joyanto@invento.com.bd>
2023-10-16 16:12:06 +0600

    fix: url exists or not checking issue

--- field_force/field_force/doctype/requisition/requisition_list.js
+++ field_force/field_force/doctype/requisition/requisition_list.js
@@ -1,11 +1,11 @@
 function check_role(){
   console.log(frappe.user.has_role("Customer"), !has_common(frappe.user_roles, ["System Manager"]),
-    !window.location.href.includes('/customer-requisition/'));
+    !window.location.href.includes('/customer-requisition'));
 
   if (frappe.user.has_role("Customer") && !has_common(frappe.user_roles, ["System Manager"])
       && !window.location.href.includes('/customer-requisition/')) {
       let url = window.location.href;
-	    window.location.href = url.replace("/requisition/", "/customer-requisition/");
+	    window.location.href = url.replace("/requisition/", "/customer-requisition");
   }
 }
 
@@ -17,7 +17,7 @@ frappe.listview_settings['Requisition'] = {
       frappe.new_doc("Requisition");
 
       if (frappe.user.has_role("Customer") && !frappe.user.has_role("System Manager")
-      && !window.location.href.includes('/customer-requisition/')) {
+      && !window.location.href.includes('/customer-requisition')) {
         let url = window.location.href;
         window.location.href = url.replace("/requisition/", "/customer-requisition/");
       }
