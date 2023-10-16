5c63e03090aea34fff1cf05832dba74c3e810dbe
Jayanta Roy <joyanto@invento.com.bd>
2023-10-16 16:28:07 +0600

    fix: url exists or not checking issue

--- field_force/field_force/doctype/requisition/requisition_list.js
+++ field_force/field_force/doctype/requisition/requisition_list.js
@@ -3,7 +3,7 @@ function check_role(){
     !window.location.href.includes('/customer-requisition'));
 
   if (frappe.user.has_role("Customer") && !has_common(frappe.user_roles, ["System Manager"])
-      && !window.location.href.includes('/customer-requisition/')) {
+      && !window.location.href.includes('/customer-requisition')) {
       let url = window.location.href;
 	    window.location.href = url.replace("/requisition/", "/customer-requisition");
   }
