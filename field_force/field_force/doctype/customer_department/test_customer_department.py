9f888298b9b8d2fe66eaeb0a6c818f4776be4549
Jayanta Roy <joyanto@invento.com.bd>
2023-11-22 13:03:32 +0600

    add: customer department doctype added and linked to partner group, customer and requsitiion

---
+++ field_force/field_force/doctype/customer_department/test_customer_department.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Invento Software Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCustomerDepartment(FrappeTestCase):
+	pass
