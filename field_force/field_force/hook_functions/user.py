e26ab5812ccd1d3269c7276a7f73b9f71afb8374
Sakib Ahamad <saakib10@yahoo.com>
2023-12-13 12:25:04 +0600

    chore: test-update

--- field_force/field_force/hook_functions/user.py
+++ field_force/field_force/hook_functions/user.py
@@ -2,41 +2,27 @@ import frappe
 
 def on_update(doc,method=None):
     if doc.department:
-        exists = frappe.db.exists('User Permission', {'user':doc.name, 'allow':"Department"})
-        if exists:
-            permission_doc = frappe.get_doc("User Permission",{'user':doc.name, 'allow':"Department"})
-            if permission_doc.for_value != doc.department:
-                permission_doc.for_value = doc.department
-                permission_doc.save()
-        else:
-            user_permission = frappe.get_doc({
-                
-                "doctype" : "User Permission",
-                "user" : doc.name,
-                "allow" : "Department",
-                "for_value" : doc.department,
-                "apply_to_all_doctypes" : 1
-            })
-            
-            
-            user_permission.save()
+        set_user_permission(doc.department,"Department",doc.name)
             
     if doc.warehouse:
-        exists = frappe.db.exists('User Permission', {'user':doc.name, 'allow':"Warehouse"})
-        if exists:
-            permission_doc = frappe.get_doc("User Permission",{'user':doc.name, 'allow':"Warehouse"})
-            if permission_doc.for_value != doc.warehouse:
-                permission_doc.for_value = doc.warehouse
-                permission_doc.save()
-        else:
-            user_permission = frappe.get_doc({
-                
-                "doctype" : "User Permission",
-                "user" : doc.name,
-                "allow" : "Warehouse",
-                "for_value" : doc.warehouse,
-                "apply_to_all_doctypes" : 1
-            })
+        set_user_permission(doc.warehouse,"Warehouse",doc.name)
             
+def set_user_permission(field_value,allow,doc_name):
+    exists = frappe.db.exists('User Permission', {'user':doc_name, 'allow':allow})
+    if exists:
+        permission_doc = frappe.get_doc("User Permission",{'user':doc_name, 'allow':allow})
+        if permission_doc.for_value != field_value:
+            permission_doc.for_value = field_value
+            permission_doc.save()
+    else:
+        user_permission = frappe.get_doc({
             
-            user_permission.save()
+            "doctype" : "User Permission",
+            "user" : doc_name,
+            "allow" : allow,
+            "for_value" : field_value,
+            "apply_to_all_doctypes" : 1
+        })
+        
+        
+        user_permission.save()
