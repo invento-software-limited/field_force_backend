d139d417a8cec36ff879ca2a4329faa00a978443
Jayanta Roy <joyanto@invento.com.bd>
2023-12-28 18:49:07 +0600

    fix: department issue

--- field_force/field_force/hook_functions/user.py
+++ field_force/field_force/hook_functions/user.py
@@ -2,11 +2,11 @@ import frappe
 
 def on_update(doc,method=None):
     if doc.department:
-        set_user_permission(doc.department,"Department",doc.name)
-            
+        set_user_permission(doc.department,"Customer Department",doc.name)
+
     if doc.warehouse:
         set_user_permission(doc.warehouse,"Warehouse",doc.name)
-            
+
 def set_user_permission(field_value,allow,doc_name):
     exists = frappe.db.exists('User Permission', {'user':doc_name, 'allow':allow})
     if exists:
@@ -16,13 +16,13 @@ def set_user_permission(field_value,allow,doc_name):
             permission_doc.save()
     else:
         user_permission = frappe.get_doc({
-            
+
             "doctype" : "User Permission",
             "user" : doc_name,
             "allow" : allow,
             "for_value" : field_value,
             "apply_to_all_doctypes" : 1
         })
-        
-        
+
+
         user_permission.save()
