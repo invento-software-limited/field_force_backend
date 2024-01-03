5cdb8b3076c34e56fb33bb2d98e189221d2f14bc
Jayanta Roy <joyanto@invento.com.bd>
2024-01-03 19:55:54 +0600

    add: usertype doctype added

---
+++ field_force/field_force/doctype/usertype/usertype.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Invento Software Limited and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('UserType', {
+	// refresh: function(frm) {
+
+	// }
+});
