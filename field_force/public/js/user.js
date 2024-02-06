d66b9165c7eea2ade444c31cb2133fdef7cdc9a9
Sakib Ahamad <saakib10@yahoo.com>
2024-02-06 09:51:55 +0600

    chore: permisiton add in js

--- field_force/public/js/user.js
+++ field_force/public/js/user.js
@@ -1,20 +1,21 @@
 frappe.ui.form.on('User', {
     refresh : function(frm) {
-
-        frm.add_custom_button(__('Generate Keys'), function(){
-            frappe.call({
-                method: "field_force.field_force.hook_functions.user.generate_keys",
-                args: {
-                    user: frm.doc.name,
-                },
-                callback: function (r) {
-                    if (r.message) {
-                        frappe.msgprint(__("Save API Secret: {0}", [r.message.api_secret]));
-                        frm.reload_doc();
-                    }
-                },
+        if (frappe.user.has_role('Admin')) {
+            frm.add_custom_button(__('Generate Keys'), function(){
+                frappe.call({
+                    method: "field_force.field_force.hook_functions.user.generate_keys",
+                    args: {
+                        user: frm.doc.name,
+                    },
+                    callback: function (r) {
+                        if (r.message) {
+                            frappe.msgprint(__("Save API Secret: {0}", [r.message.api_secret]));
+                            frm.reload_doc();
+                        }
+                    },
+                });
             });
-        });
+        }   
     },
     onload: function (frm){
         console.log("Custom js working");
